73e04849b28617ca4f457f512135d01f65f6a7bb
Esraaj Sarkar Gupta <esraaj.sarkar.gupta.24@gmail.com>
2024-07-24 12:22:41 +0530

    Reorganisation

---
+++ experimental_conditions/Gravitational_Orbits/24july_gorbit_1/main.py
@@ -0,0 +1,41 @@
+import physics as p
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0 , 5000 , 250) 
+# initial time, final time, number of frames in between
+
+b1v = np.array([0,0.2583466663225984])
+b1x = np.array([100,0])
+b1 = p.body("b1", b1v, b1x, 1, 0, 1, "blue")
+
+b2v = np.array([0,0])
+b2x = np.array([0,0])
+b2 = p.body("b2", b2v, b2x, 1, 0, 1e+11, "green")
+
+for i in range(len(t)-1):
+    
+    if i % 100 == 0:
+        print(f"> Instance {i}")
+    
+    plt.figure()
+    plt.title(f"Time instance {i}")
+    
+    d = 500
+    plt.xlim(-d , d)
+    plt.ylim(-d , d)
+
+    plt.scatter(b1.x[0] , b1.x[1] , color = b1.color)
+    plt.scatter(b2.x[0] , b2.x[1] , color = b2.color)
+
+    del_t = t[i+1] - t[i]
+    net_a_b1 = b1.net_a(b2)
+    b1.update(net_a_b1, del_t)
+    
+    if b1.v[0] > 1e5 or b1.v[1] > 1e5:
+        print(f'[!!] Extreme: {b1.v}')
+
+    plt.grid(True)
+    #plt.show()
+    plt.savefig(f"frames/frame_{i}.png")
+    plt.close()
